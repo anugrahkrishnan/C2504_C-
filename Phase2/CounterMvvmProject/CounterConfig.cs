9b7a68d9e8222e59b3033e07a7d45bde2479bad3
anugrahkrishnan <64000608+anugrahkrishnan@users.noreply.github.com>
2024-10-06 12:43:13 +0530

    Added counter mvvm

---
+++ Phase2/CounterMvvmProject/CounterConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterMvvmProject
+{
+    static class CounterConfig
+    {
+        public static CounterViewModel ViewModel { get; set; }
+
+        static CounterConfig()
+        {
+            ViewModel = new CounterViewModel(); 
+        }
+            
+    }
+}
---
+++ Phase2/CounterMvvmProject/CounterViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CounterMvvmProject
+{
+    public class CounterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Counter _displayCount;
+        public Counter DisplayCount
+        {
+            get { return _displayCount; }
+            set
+            {
+                _displayCount = value;
+                OnPropertyChanged(nameof(DisplayCount));
+            }
+        }
+
+        public ICommand IncrementCount { get; }
+        public ICommand DecrementCount { get; }
+
+        public CounterViewModel()
+        {
+            DisplayCount = new Counter { Count = 0 };
+            IncrementCount = new RelayCommand(Increment);
+            DecrementCount = new RelayCommand(Decrement);
+        }
+
+        public void Increment()
+        {
+            DisplayCount.Count++;
+            DisplayCount = DisplayCount;
+        }
+        public void Decrement()
+        {
+            DisplayCount.Count--;
+            DisplayCount = DisplayCount;
+
+        }
+    }
+}
